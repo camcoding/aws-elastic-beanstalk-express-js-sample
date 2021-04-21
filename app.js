3cd36bcefea4ae26abd1044e51d45473a6b3b3e0
Cameron Michaelson <US23518@melaleuca.net>
2021-04-20 21:37:40 -0600

    changed name again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello There Chubs!'));
+app.get('/', (req, res) => res.send('Hello There Idaho!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
